b41bef17fffe8ccef73abf49dce586a15848075b
sanxback89 <159859513+sanxback89@users.noreply.github.com>
2024-11-07 10:48:59 +0900

    app.py

--- app.py
+++ app.py
@@ -683,13 +683,6 @@ def main():
                                             type=["xlsx", "xls", "png", "jpg", "jpeg", "jfif", "zip"], 
                                             accept_multiple_files=True,
                                             key="file_uploader")
-        with col2:
-            if st.button("Clear Files"):
-                # 파일 업로더와 분석 결과 초기화
-                st.session_state.file_uploader = []
-                st.session_state.analysis_results = {}
-                st.session_state.image_categories = defaultdict(lambda: defaultdict(list))
-                st.rerun()
         
         if uploaded_files and selected_options:  # 파일과 분석 항목이 모두 선택된 경우
             images = []
